5b0f56e3aaa7673846ee89e04648799280590156
AIMLjorney <kumawatking9982@gmail.com>
2025-01-09 16:35:41 +0530

    outlier scannig

--- python+numpy+pandas.py
+++ python+numpy+pandas.py
@@ -8,35 +8,64 @@
 # filter_BMW1.dropna(axis=0,how='all',inplace=True)
 # print(filter_BMW1)
 # print()
+# import pandas as pd
+# import numpy as np
+# files = pd.read_csv("C:/Users/hp5cd/Downloads/train.csv")
+# file = files.loc[0:10, ['Age', 'Pclass', 'Survived']]
+# if file['Age'].isnull().any():
+#     file['Age'] = file['Age'].fillna(file['Age'].median())
+# corrraltion1 = file['Age'].cov(file['Pclass'])
+# corrraltion2 = file['Pclass'].cov(file['Survived'])
+# corrraltion3 = file['Survived'].cov(file['Age'])
+# print("corelation of the age and pclass",corrraltion1)
+# print("correlation of the pclass and survived",corrraltion2)
+# print("correlation of the  survived and age",corrraltion3)
+# print(file.columns)
+# files = files.loc[0:20,['Sex','Embarked']]
+# def applyy(num):
+#     if num == 'male':
+#         return 0
+#     else:
+#         return 1
+
+# def changed(val):
+#     if val == 'S':
+#         return 1
+#     elif(val=='Q'):
+#         return 2
+#     elif(val=='C'):
+#         return 3
+    
+# files['Sex'] = files['Sex'].apply(applyy)
+# files['Embarked'] = files['Embarked'].apply(changed)
+# print(files['Embarked'],files['Sex'])
+# print(files)    
 import pandas as pd
 import numpy as np
-files = pd.read_csv("C:/Users/hp5cd/Downloads/train.csv")
-file = files.loc[0:10, ['Age', 'Pclass', 'Survived']]
-if file['Age'].isnull().any():
-    file['Age'] = file['Age'].fillna(file['Age'].median())
-corrraltion1 = file['Age'].cov(file['Pclass'])
-corrraltion2 = file['Pclass'].cov(file['Survived'])
-corrraltion3 = file['Survived'].cov(file['Age'])
-print("corelation of the age and pclass",corrraltion1)
-print("correlation of the pclass and survived",corrraltion2)
-print("correlation of the  survived and age",corrraltion3)
-print(file.columns)
-files = files.loc[0:20,['Sex','Embarked']]
-def applyy(num):
-    if num == 'male':
-        return 0
-    else:
-        return 1
+import matplotlib.pyplot as py
+file = pd.read_csv("C:/Users/hp5cd/Downloads/used_car_dataset.csv")
+# print(file)
+# print(file.describe())
+# print(file.tail())
+def ma(lis):
+    mean = lis.mean()
+    medium = lis.median()
+    mode = lis.mode().values[0]
+    return(mean,medium,mode)
+(mean,median,mode) = ma(file['Year'])
+print(mean)
+print(median)
+print(mode)
 
-def changed(val):
-    if val == 'S':
-        return 1
-    elif(val=='Q'):
-        return 2
-    elif(val=='C'):
-        return 3
-    
-files['Sex'] = files['Sex'].apply(applyy)
-files['Embarked'] = files['Embarked'].apply(changed)
-print(files['Embarked'],files['Sex'])
-print(files)    
+
+quar1 = file['Year'].quantile(q=0.25)
+quar3 = file['Year'].quantile(q=0.75)
+quar2 = quar3-quar1
+lower_boundry = quar1-(1.5*quar2)
+upper_boundery = quar3+(1.5*quar2)
+print(quar3)
+print(lower_boundry)
+print(upper_boundery)
+print(file[(file['Year']<lower_boundry) | (file['Year']>upper_boundery)])
+py.hist(file['Year'])
+py.show()
